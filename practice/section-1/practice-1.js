d0ceff37163be2ceeeec64a39d9aca8e7b275c27
Dan Luigi Tating <dan.luigi.tating@oocl.com>
2019-09-12 17:31:30 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var similarNumbers = new Array();
+
+  for(var i = 0; i<collectionA.length ; i++)
+  {
+	  for(var j = 0; j<collectionB.length ; j++)
+	  {
+		  if (collectionA[i] == collectionB[j])
+		  {
+			  similarNumbers.push(collectionA[i]);
+			  break;
+		  }
+	  }
+   }
+  
+  return similarNumbers;
 }
